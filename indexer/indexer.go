ed5701c10924d4f083475e7c3ac599b6cb9b3186
Aarón García <aaron.gavez@gmail.com>
2024-02-09 03:08:57 -0600

    Remove multiple return values from index batch

--- indexer/indexer.go
+++ indexer/indexer.go
@@ -57,30 +57,27 @@ func (i Indexer) Index(news *nwelastic.News) error {
 }
 
 // IndexBatch calls /index/batch and returns the total amount of indexed records, the last indexed id, and an error if exists
-func (i Indexer) IndexBatch(news []*nwelastic.News) (int, int, error) {
+func (i Indexer) IndexBatch(news []*nwelastic.News) error {
 	newsJson, err := json.Marshal(news)
 	if err != nil {
-		return -1, -1, errors.Wrap(err, "marshaling news item")
+		return errors.Wrap(err, "marshaling news item")
 	}
 	resp, err := http.Post(i.urlWithAuth("/index/batch"), i.contentType, bytes.NewReader(newsJson))
 	if err != nil {
-		return -1, -1, errors.Wrap(err, "calling /index/batch")
+		return errors.Wrap(err, "calling /index/batch")
 	}
 
 	if resp.StatusCode >= 200 && resp.StatusCode <= 299 {
-		respApi, err := handleResponse[IndexBatchData](resp)
-		if err != nil {
-			return -1, -1, errors.Wrap(err, "handling response")
-		}
-		return respApi.Data.TotalIndexed, respApi.Data.LastIndex, nil
+		handleEmptyResponse(resp)
+		return nil
 	}
 
 	respApi, err := handleErrorResponse[IndexBatchData](resp)
 	if err != nil {
-		return -1, -1, errors.Wrap(err, "handling error response")
+		return errors.Wrap(err, "handling error response")
 	}
 
-	return respApi.Error.Data.TotalIndexed, respApi.Error.Data.LastIndex, errors.New(respApi.Error.Message)
+	return errors.New(respApi.Error.Message)
 }
 
 func (i Indexer) Ping() error {
