715fd161e3b20fe8cb59cb7809df15b157a25e9c
Aaron G. <aaron.gavez@gmail.com>
2026-03-04 15:54:07 -0600

    add support for passing optional nats.Options to `JetStream` in `New`

--- natsprocessor/nats_processor.go
+++ natsprocessor/nats_processor.go
@@ -46,8 +46,8 @@ type OptsWithJs struct {
 	Bucket    string
 }
 
-func New(opts OptsWithConf) (NatsProcessor, error) {
-	js, err := opts.NatsConfig.JetStream()
+func New(opts OptsWithConf, options ...nats.Option) (NatsProcessor, error) {
+	js, err := opts.NatsConfig.JetStream(options...)
 	if err != nil {
 		return NatsProcessor{}, err
 	}
